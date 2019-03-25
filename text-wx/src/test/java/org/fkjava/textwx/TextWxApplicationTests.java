93d71b6878cac1ca1c2070ec323742eb0b26eb50
tjlwand <48503109+tjlwand@users.noreply.github.com>
2019-03-25 13:36:40 +0800

    Add files via upload

---
+++ text-wx/src/test/java/org/fkjava/textwx/TextWxApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fkjava.textwx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextWxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
